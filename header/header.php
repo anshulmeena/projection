1cf84fc1860e0bf93311bea85739c64ed419d444
Anshul Meena <linuxanshul@gmail.com>
2012-09-09 20:49:57 -0400

    Header dir. for mobile and desktop version of header of the website.

---
+++ header/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Human Centered Computing</title>
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.js"></script>
+
+</head>
+<body>
+	<!-- main menu bar -->
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a data-target=".nav-collapse" data-toggle="collapse"
+					class="btn btn-navbar"> <span class="icon-bar"></span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span>
+				</a> <a href="#" class="brand">Human Centered Computing</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
