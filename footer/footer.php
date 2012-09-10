1c98fbb89562a0cadc1622e98fa4548d4fe6cdb7
Anshul Meena <linuxanshul@gmail.com>
2012-09-09 20:49:14 -0400

    Footer dir. for mobile and desktop version of website.

---
+++ footer/footer.php
@@ -0,0 +1,33 @@
+<!-- global 1080px footer -->
+<hr>
+<footer class="footer">
+	<div class="container">
+		<p class="pull-right">
+			<a href="#">Back to top</a>
+		</p>
+		<p>
+			Designed and built with all the love in the world <a target="_blank"
+				href="http://twitter.com/twitter">@twitter</a> by <a target="_blank"
+				href="http://twitter.com/mdo">@mdo</a> and <a target="_blank"
+				href="http://twitter.com/fat">@fat</a>.
+		</p>
+		<p>
+			Code licensed under the <a target="_blank"
+				href="http://www.apache.org/licenses/LICENSE-2.0">Apache License
+				v2.0</a>. Documentation licensed under <a
+				href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.
+		</p>
+		<p>
+			Icons from <a href="http://glyphicons.com">Glyphicons Free</a>,
+			licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC
+				BY 3.0</a>.
+		</p>
+		<ul class="footer-links">
+			<li><a href="http://blog.getbootstrap.com">Read the blog</a></li>
+			<li><a href="https://github.com/twitter/bootstrap/issues?state=open">Submit
+					issues</a></li>
+			<li><a href="https://github.com/twitter/bootstrap/wiki">Roadmap and
+					changelog</a></li>
+		</ul>
+	</div>
+</footer>
